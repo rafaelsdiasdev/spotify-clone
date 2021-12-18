1ce2ec4cced47738d30adf5da628e9ecb9293f00
Rafael Dias <rsdias84@gmail.com>
2021-12-18 09:46:32 -0300

    test redirecionamento

--- next.config.js
+++ next.config.js
@@ -2,11 +2,11 @@ const withImages = require('next-images');
 // const createEnvFile = require('./environment-builder');
 
 module.exports = withImages({
+  reactStrictMode: true,
   target: 'serverless',
   env: {
     API_URL: process.env.API_URL,
   },
-  reactStrictMode: true,
   images: {
     domains: ['rsdias-storage.s3.amazonaws.com', 'i.scdn.co'],
   },
