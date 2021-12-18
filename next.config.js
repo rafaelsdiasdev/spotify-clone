5accbba7d7c034865ed1d3cc3d54218717640aaf
Rafael Dias <rsdias84@gmail.com>
2021-12-18 14:14:44 -0300

    test redirecionamento

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = withImages({
   target: 'serverless',
   env: {
     API_URL: process.env.API_URL,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
   },
   images: {
     domains: ['rsdias-storage.s3.amazonaws.com', 'i.scdn.co'],
