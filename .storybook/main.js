34284cef40a1764a0fbf862a1ac291d25fa2493c
Rafael Dias <rsdias84@gmail.com>
2022-01-22 15:59:24 -0300

    add eslint no ci/cd

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,27 +1,31 @@
 module.exports = {
   core: {
-    builder: "webpack5",
+    builder: 'webpack5',
   },
   stories: [
     '../stories/**/*.stories.mdx',
     '../components/**/**/*.stories.@(js|mdx)',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    // '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-docs', '@storybook/addon-links', '@storybook/addon-essentials', "storybook-addon-pseudo-states"],
-  
-  webpackFinal: config => { 
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-pseudo-states',
+  ],
+
+  webpackFinal: (config) => {
     const rules = config.module.rules;
     const fileLoaderRule = rules.find((rule) => rule.test.test('.svg'));
     fileLoaderRule.exclude = /\.svg$/;
 
-    console.log(rules)
-  
+    console.log(rules);
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-  
-    return config;
-  } 
-};
 
+    return config;
+  },
+};
